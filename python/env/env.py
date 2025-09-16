d0f8ad140774d7d5dd7307411373755de8248c79
purod <djyjin@gmail.com>
2025-09-15 21:40:58 -0400

    Update env.py

--- python/env/env.py
+++ python/env/env.py
@@ -8,9 +8,21 @@
 # show all rows or the first 10 rows, for all, you use None
 pd.set_option("display.max_rows",10)   
 
+# Set default figure size and DPI
+mpl.rcParams['figure.dpi'] = 200         # Higher DPI = better quality
+mpl.rcParams['savefig.dpi'] = 300        # When saving figures
+mpl.rcParams['figure.figsize'] = (4, 2)  # Default size for all figures
+mpl.rcParams['font.size'] = 12           # Default font size
+
+# modules
+import pyreadr
+import pandas as pd
+import seaborn as sns
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 from src.profilereport_config import custom_config
-
 
 profile = ProfileReport(
     airport, config=custom_config,
